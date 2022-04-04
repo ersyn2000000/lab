0bdc2bd4668ec964fd309155487ca3330f3bbe4a
ersyn2000000 <geralt606@gmail.com>
2022-04-04 23:07:27 +0600

    master

---
+++ script/slider.js
@@ -0,0 +1,22 @@
+function nextSlide() {
+    goToSlide(currentSlide+1);
+}
+function previousSlide() {
+    goToSlide(currentSlide-1);
+}
+function goToSlide(n) {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (n+slides.length)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+
+var next = document.getElementById('next');
+var previous = document.getElementById('previous');
+next.onclick = function() {
+    pauseSlideshow();
+    nextSlide();
+};
+previous.onclick = function() {
+    pauseSlideshow();
+    previousSlide();
+};
